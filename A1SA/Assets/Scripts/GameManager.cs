915f4c6d9af45917571249833ab9290963b3d191
jelly1702 <urin1702@gmail.com>
2024-04-17 16:17:42 +0900

    Revert "EndPanel Animation"
    This reverts commit cbf924076cce99830f3ef3f1bd8b3ba5364a6e72.

--- A1SA/Assets/Scripts/GameManager.cs
+++ A1SA/Assets/Scripts/GameManager.cs
@@ -141,7 +141,6 @@ public class GameManager : MonoBehaviour
             //게임 종료
             if (cardCount == 0)
             {
-                endAnim.SetBool("EndPanel", true);
                 TimeScore();
                 reminingTime -= time;
                 GameManager.Instance.GameOver();
