074ac09164515a4abcdd6f1aa895d6481a9dd4d7
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-04-17 11:19:06 +0900

    Next Button Load Scene

--- A1SA/Assets/Scripts/RetryButton.cs
+++ A1SA/Assets/Scripts/RetryButton.cs
@@ -12,4 +12,23 @@ public class RetryButton : MonoBehaviour
     {
         SceneManager.LoadScene("StartScene");
     }
+
+    public void GoStage1()
+    {
+        SceneManager.LoadScene("Stage1Scene");
+    }
+
+    public void GoStage2()
+    {
+        SceneManager.LoadScene("Stage2Scene");
+    }
+
+    public void GoStage3()
+    {
+        SceneManager.LoadScene("Stage3Scene");
+    }
+    public void GoStage4()
+    {
+        SceneManager.LoadScene("Stage4Scene");
+    }
 }
