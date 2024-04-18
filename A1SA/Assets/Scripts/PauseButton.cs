a0f05c3e5cc744a60929468ab1d835a6546ace97
jelly1702 <urin1702@gmail.com>
2024-04-18 13:54:17 +0900

    Squashed commit of the following:
    commit f0c67d8a9daad02f18a1dc19aea691efbbe0c10f
    Merge: 0eb5009 d2cc0ac
    Author: jelly1702 <urin1702@gmail.com>
    Date:   Thu Apr 18 13:52:47 2024 +0900
    
        Merge branch 'dev' into Feature/AudioMixer
    
    commit 0eb500995350ecedfe97baa1c31231863383c7c2
    Author: jelly1702 <urin1702@gmail.com>
    Date:   Thu Apr 18 13:51:42 2024 +0900
    
        AudioMixer,UI

--- A1SA/Assets/Scripts/PauseButton.cs
+++ A1SA/Assets/Scripts/PauseButton.cs
@@ -5,26 +5,15 @@ using UnityEngine.UI;
 
 public class PauseButton : MonoBehaviour
 {
-    public Sprite pauseImg;
-    public Sprite playImg;
-
+    public GameObject stopPanel;
     public void PauseGame()
     {
-        //일시정지 상태인 경우
-        if(Time.timeScale == 0f)
+        //시간이 흐르고 있다면
+        if(Time.timeScale == 1f)
         {
-            //일시정지 상태를 해제하고 시간을 다시 진행
-            Time.timeScale = 1f;
-            //pauseImg를 불러온다
-            GetComponent<Image>().sprite = pauseImg;
-        }
-        //일시정지 상태가 아닌경우
-        else
-        {
-            //일시정지 상태로 만들고 시간을 멈춤
+            //시간을 멈춤
             Time.timeScale = 0f;
-            //playImg를 불러온다
-            GetComponent<Image>().sprite = playImg;
+            stopPanel.SetActive(true);
         }
     }
 }
